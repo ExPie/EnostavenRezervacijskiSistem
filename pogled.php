b9efc36adbf19f4dff5da24d2d55a83bc39bbf18
Pivkst <jan.t.pivk@gmail.com>
2015-05-08 09:22:09 +0200

    Facelift
    Izboljšan izgled kode in končnega rezultata

--- pogled.php
+++ pogled.php
@@ -1,59 +1,64 @@
 <?php
-	//print_r($result);
+	function izris($conn, $kljuc=NULL)
+	{
+		if($kljuc==NULL)
+		{
+			$sql = "SELECT * FROM dejavnost;";
+			$result = $conn -> query($sql);
+			$result = $result -> fetch_all(MYSQLI_ASSOC);
+		}
+		else
+		{
+			$sql = "SELECT * FROM dejavnost WHERE (
+				naslovD LIKE '%".$kljuc."%' OR
+				MentorjiD LIKE '%".$kljuc."%' OR
+				OrgOblikaD LIKE '%".$kljuc."%' OR
+				NadarjenostD LIKE '%".$kljuc."%' OR
+				OpombeD LIKE '%".$kljuc."%'
+			);";
+			$result = $conn -> query($sql);
+			$result = $result -> fetch_all(MYSQLI_ASSOC);
+		}
+		if(!$result)
+			echo'<table><tr><td>Ni  zadetkov.</tr></td></table>';
+		else
+		{
+			echo'<table>';
+			echo'<tr><td>Ime dejavnosti</td><td>Oblika</td><td>Namenjeno</td></tr>';
+			foreach($result as $el)
+			{
+				echo'<tr><td>'.$el["naslovD"].'</td><td>'.$el["OrgOblikaD"].'</td><td>'.$el["PrimernostD"].'</td></td>';
+			}
+			echo'</table>';
+		}
+	}
+	
 	echo'
+	<style>
+		table, th, td
+		{
+			border: 1px solid black;
+			border-collapse: collapse;
+			background-color: white;
+			padding: 5px;
+		}
+	</style>
 	<form method="POST" action="pogled.php">
 	<input type="text" name="search">
 	<input type="submit" value="Iskanje">
 	</form>';
 	$conn = mysqli_connect('localhost', 'root', '', 'dejavnosti');
-	if(count($_POST)==0)
+	if (!$conn)
 	{
-		if (!$conn)
-		{
-   			$returnString="Connection failed: " . mysqli_connect_error();
- 		}
-		else
-		{
-    		$sql = "SELECT * FROM dejavnost;";
-			$result = $conn -> query($sql);
-  			$result = $result -> fetch_all(MYSQLI_ASSOC);
- 		}
-		echo'<h1>Dejavnosti:<h1><br>';
-		echo'<table>';
-		foreach($result as $el)
-		{
-			echo'<tr><td>'.$el["naslovD"].'</td></td>';
-		}
-		echo'</table>';
+		$returnString="Podatkovna baza je nedosegljiva: " . mysqli_connect_error();
+		echo $returnString.'<br/>';
 	}
 	else
 	{
-	  	if (!$conn)
-		{
-   			$returnString="Connection failed: " . mysqli_connect_error();
- 		}
+		echo'<h1>Dejavnosti:<h1>';
+		if(count($_POST)==0)
+			izris($conn);
 		else
-		{
-    		$sql = "SELECT * FROM dejavnost WHERE (
-				naslovD LIKE '%".$_POST["search"]."%' OR
-				MentorjiD LIKE '%".$_POST["search"]."%' OR
-				OrgOblikaD LIKE '%".$_POST["search"]."%' OR
-				NadarjenostD LIKE '%".$_POST["search"]."%' OR
-				OpombeD LIKE '%".$_POST["search"]."%'
-			);";
-			$result = $conn -> query($sql);
-			if(!$result)
-				echo'<table><tr><td>Ni  zadetkov.</tr></td></table>';
-			else
-			{
-				$result = $result -> fetch_all(MYSQLI_ASSOC);
-				echo'<table>';
-				foreach($result as $el)
-				{
-					echo'<tr><td>'.$el["naslovD"].'</td></td>';
-				}
-				echo'</table>';
-			}
- 		}
+			izris($conn, $_POST["search"]);
 	}
 ?>
